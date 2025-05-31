edc6d830ff1b4ee8c0c854d88ea1da08c7c1be7e
Viktor <30930109+vokic@users.noreply.github.com>
2025-05-31 12:52:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//  Simplest JS accordion implementation
+
+document.querySelectorAll(".accordion-header").forEach((header) => {
+  header.addEventListener("click", () => {
+    const item = header.parentElement;
+    item.classList.toggle("active");
+  });
+});
